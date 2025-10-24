3e63f1c347871005dab18c4a43b44de62c56dddc
TeaRediT <vlad.bobro.02@gmail.com>
2025-10-24 22:05:25 +0300

    final fixes

--- js/task-3.js
+++ js/task-3.js
@@ -2,5 +2,7 @@ const inputEl = document.querySelector('#name-input');
 const spanEl = document.querySelector('#name-output');
 
 inputEl.addEventListener('input', event => {
-  spanEl.textContent = event.currentTarget.value;
+  const text = event.currentTarget.value.trim();
+  if (text.length > 0) spanEl.textContent = text;
+  else spanEl.textContent = 'Anonymous';
 });
